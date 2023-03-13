8406a45ecdb5554410ab9561686236560e3f77e1
coderrob22 <robertknott7@gmail.com>
2023-03-13 09:51:07 -0400

    day1 project for 100days of code

---
+++ day1project.py
@@ -0,0 +1,4 @@
+print("Hi, welcome to my band name generator!!!")
+city_name = input("What's the name of the city you grew up in? \n")
+pet_name = input("What is the name of your pet? \n")
+print("Your band's name is " + city_name + " " + pet_name)
