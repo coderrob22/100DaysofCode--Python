51409835aa1dabf1256e4145d03256f053431746
coderrob22 <robertknott7@gmail.com>
2023-05-03 10:51:07 -0400

    hangman is done

--- day7hangman_part5.py
+++ day7hangman_part5.py
@@ -1,8 +1,6 @@
 import random
 import hangman_words
-import hangman_art
-
-#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+from hangman_art import stages, logo
 
 chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
@@ -10,11 +8,7 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
-print(hangman_art.logo)
-
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+print(logo)
 
 #Create blanks
 display = []
@@ -24,22 +18,18 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
-
     if guess in display:
         print(f"You have guessed {guess} already. Try again.")
 
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
-        print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+       # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
 
     #Check if user is wrong.
-    if guess not in chosen_word:
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
-        
+    if guess not in chosen_word:  
         print(f"You guessed {guess}, but that's not in the word, so you lose a life. Sorry.")
 
         lives -= 1
@@ -55,5 +45,4 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
-    print(hangman_art.stages[lives])
+    print(stages[lives])
