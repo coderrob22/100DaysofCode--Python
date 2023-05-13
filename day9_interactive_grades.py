235da1689f0d915f8a24e87489680f7a0a4abf2c
coderrob22 <robertknott7@gmail.com>
2023-05-13 08:51:07 -0400

    grades and dictionaries

---
+++ day9_interactive_grades.py
@@ -0,0 +1,25 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+for grade in student_scores:
+  if student_scores[grade] > 90:
+    student_grades[grade] = "Outstanding"
+  elif student_scores[grade] > 80:
+    student_grades[grade] = "Exceeds Expectations"
+  elif student_scores[grade] > 70:
+    student_grades[grade] = "Acceptable"
+  else:
+    student_grades[grade] = "Fail"
+  
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
