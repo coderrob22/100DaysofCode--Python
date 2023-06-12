18b32a6b7a7688ca0e79e8fc65788d4a4624d11f
coderrob22 <robertknott7@gmail.com>
2023-06-12 04:43:03 -0400

    almost finished with game logic

--- day14_higherLower_project.py
+++ day14_higherLower_project.py
@@ -1,44 +1,58 @@
 import higher_lower_art
 import higher_lower_data
 import random
+import os
 
 # Show the logo
+os.system('clear')
 print(higher_lower_art.logo)
 
-
-## Randomly select info from the API
-#Select A
-pick_1 = random.choice(higher_lower_data.data)
-
-#Select B
-pick_2 = random.choice(higher_lower_data.data)
-
-if pick_1 == pick_2:
-    pick_2 = random.choice(higher_lower_data.data)
+def check_answer(user_guess, num_follower_1, num_follower_2):
+    """This takes the user's guess and compares it to the larger followers to see if the user guessed correctly"""
+    if num_follower_1 > num_follower_2:
+        return user_guess == 'a'
+    else:
+        return user_guess == 'b'
 
 score = 0
+proceed = True   
+pick_2 = random.choice(higher_lower_data.data)
+
+while proceed:
+
+    ## Randomly select info from the API
+    #Select A
+    pick_1 = pick_2
+
+    #Select B
+    pick_2 = random.choice(higher_lower_data.data)
+
+    while pick_1 == pick_2:
+        pick_2 = random.choice(higher_lower_data.data)
+
+    #Show info from A
+    print(f"Compare A: {pick_1['name']}, a {pick_1['description']}, from {pick_1['country']}.\n")
+
+    print(higher_lower_art.vs)
+
+    #Show info from B
+    print(f"Compare B: {pick_2['name']}, a {pick_2['description']}, from {pick_2['country']}.\n")
 
 
-    
+    # Ask the user to guess the answer
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        
+    #Compare A and B and see which has more followers
+    pick1_followers = pick_1['follower_count']
+    pick2_followers = pick_2['follower_count']
 
-#Show info from A
-print(f"Compare A: {pick_1['name']}, a {pick_1['description']}, from {pick_1['country']}.\n")
+    correct = check_answer(guess, pick1_followers, pick2_followers)
 
-print(higher_lower_art.vs)
+    if correct:
+        score += 1
+        print(f"That was correct. Your score is {score}")
+        
 
-#Show info from B
-print(f"Compare B: {pick_2['name']}, a {pick_2['description']}, from {pick_2['country']}.\n")
-
-if input("Who has more followers? Type 'A' or 'B': ") == answer:
-    score += 1
-else: 
-    print("Sorry that is incorrect. Game Over")
-    GameOver
-
-
-
-
-
-#Compare A and B and see which has more followers
-#Receive input
-#Compare the input to the answer... if the input and answer are the same...increment score...if not, game over
+    else:
+        print(f"Incorrect! Sorry, not sorry, game over. Your score was {score}")
+        proceed = False
