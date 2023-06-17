2263d2dda9b851f74cfd409932d9d3abe03aac80
coderrob22 <robertknott7@gmail.com>
2023-06-17 04:43:03 -0400

    creating coffee maker, 2 steps down

---
+++ day15_project_coffeeMaker.py
@@ -0,0 +1,45 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+# TODO: 1. Prompt user by asking "What would you like?"
+coffee_preference = input("What would you like? espresso/latte/cappuccino: ").lower()
+
+# TODO: 2. Turn off the Coffee Machine by entering 'off' in the prompt
+# TODO: 3. Print report
+if coffee_preference == 'report':
+    print(f"Water: {resources['water']}ml \nMilk: {resources['milk']}ml \nCoffee: {resources['coffee']}g \nMoney: $0")
+
+# TODO: 4. Check resource sufficiency
+# TODO: 5. Process coins
+# TODO: 6. Check if transaction successful
+# TODO: 7. Make coffee
