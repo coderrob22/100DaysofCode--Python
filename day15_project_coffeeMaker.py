49513700d703e92677173ee45ee1d12cfc5ff4dd
coderrob22 <robertknott7@gmail.com>
2023-06-18 04:43:03 -0400

    made changes to the coffee maker, also completed addl app requirement

--- day15_project_coffeeMaker.py
+++ day15_project_coffeeMaker.py
@@ -30,15 +30,27 @@ resources = {
     "coffee": 100,
 }
 
+turn_off = False
+total_money = 0
+
+while not turn_off:
+    coffee_preference = input("What would you like? espresso/latte/cappuccino: ").lower()
+
+    if coffee_preference == 'report':
+        print(f"Water: {resources['water']}ml \nMilk: {resources['milk']}ml \nCoffee: {resources['coffee']}g \nMoney: ${total_money}")
+    elif coffee_preference == 'off':
+        turn_off = True
+    elif coffee_preference == 'espresso':
+        if resources["water"] >= MENU["espresso"]["ingredients"]["water"] and resources["coffee"] >= MENU["espresso"]["ingredients"]["coffee"]:
+            print('Please insert coins.')
+
+
+
+
 
 # TODO: 1. Prompt user by asking "What would you like?"
-coffee_preference = input("What would you like? espresso/latte/cappuccino: ").lower()
-
 # TODO: 2. Turn off the Coffee Machine by entering 'off' in the prompt
 # TODO: 3. Print report
-if coffee_preference == 'report':
-    print(f"Water: {resources['water']}ml \nMilk: {resources['milk']}ml \nCoffee: {resources['coffee']}g \nMoney: $0")
-
 # TODO: 4. Check resource sufficiency
 # TODO: 5. Process coins
 # TODO: 6. Check if transaction successful
