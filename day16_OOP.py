dcabe19bb63ca1c95da2c48f27596be9cced815f
coderrob22 <robertknott7@gmail.com>
2023-06-20 04:43:03 -0400

    exploring turle module and the attributes and methods of OOP

---
+++ day16_OOP.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+turner = Turtle()
+
+my_screen = Screen()
+
+my_screen.canvheight
+turner.shape("turtle")
+turner.color("cyan")
+
+print(my_screen)
+my_screen.exitonclick()
