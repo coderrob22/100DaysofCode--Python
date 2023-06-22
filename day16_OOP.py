1e648bd15af99099fc282316c8d76908981912ab
coderrob22 <robertknott7@gmail.com>
2023-06-22 04:43:03 -0400

    adding new pkg PrettyTable to the project

--- day16_OOP.py
+++ day16_OOP.py
@@ -1,4 +1,17 @@
 from turtle import Turtle, Screen
+import prettytable
+
+turner = Turtle()
+
+my_screen = Screen()
+
+my_screen.canvheight
+turner.shape("turtle")
+turner.color("cyan")
+turner.fd(100)
+
+print(my_screen)
+my_screen.exitonclick()
 
 turner = Turtle()
 
