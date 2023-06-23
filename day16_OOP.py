c734c57c60d0882aa850f9c61836b80b47dee3bf
coderrob22 <robertknott7@gmail.com>
2023-06-23 04:43:03 -0400

    initialize new class--table before adding to it

--- day16_OOP.py
+++ day16_OOP.py
@@ -1,26 +1,18 @@
 from turtle import Turtle, Screen
 import prettytable
 
-turner = Turtle()
+# turner = Turtle()
+#
+# my_screen = Screen()
+#
+# my_screen.canvheight
+# turner.shape("turtle")
+# turner.color("cyan")
+# turner.fd(100)
+#
+# print(my_screen)
+# my_screen.exitonclick()
 
-my_screen = Screen()
+table = prettytable.PrettyTable()
 
-my_screen.canvheight
-turner.shape("turtle")
-turner.color("cyan")
-turner.fd(100)
-
-print(my_screen)
-my_screen.exitonclick()
-
-turner = Turtle()
-
-my_screen = Screen()
-
-my_screen.canvheight
-turner.shape("turtle")
-turner.color("cyan")
-turner.fd(100)
-
-print(my_screen)
-my_screen.exitonclick()
+print(table)
