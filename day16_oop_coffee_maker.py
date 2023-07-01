18d1f53a6e02a2df29825aad1d96fddb19702bc5
coderrob22 <robertknott7@gmail.com>
2023-07-01 04:43:03 -0400

    object oriented programming for coffee maker

--- day16_oop_coffee_maker.py
+++ day16_oop_coffee_maker.py
@@ -9,9 +9,11 @@ menu = Menu()
 #menu_item = MenuItem()
 
 is_on = True
-choice = input(f"What kind of coffee would you like? Type {menu.get_items()} ")
+profit = 0
 
 while is_on:
+    choice = input(f"What kind of coffee would you like? Type {menu.get_items()} ")
+    
     if choice == 'report':
         coffee_maker.report()
         money_machine.report()
@@ -20,3 +22,6 @@ while is_on:
         is_on = False
     else:
         drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
