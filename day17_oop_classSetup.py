759ccb0a0784b9a3ec6b59ad2fc1d39e3ad7b632
coderrob22 <robertknott7@gmail.com>
2023-07-03 04:43:03 -0400

    OOP creating classes and making objects

--- day17_oop_classSetup.py
+++ day17_oop_classSetup.py
@@ -1,4 +1,4 @@
 class User:
     pass #This is how you can create a class and leave it alone w/o adding anything to it.
 
-user_1= User()  #this is how you initialize a class
+user_1= User()  #this is how you creat a new object from that class. You initialize the with the      parenthesis.
