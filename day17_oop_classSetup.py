38d7a49c557d09bb8d3255ad718ccd4526c82bc1
coderrob22 <robertknott7@gmail.com>
2023-07-06 04:43:03 -0400

    adding default values

--- day17_oop_classSetup.py
+++ day17_oop_classSetup.py
@@ -9,7 +9,8 @@ class Using:
     def __init__(self, user_id, user_name):  #This init function is the constructor!
         self.id = user_id
         self.name = user_name
-
+        self.followers = 0   #This is how you give a default value to an attribute for every object.
+                            # There is no need to add it to the paramenter. It has a value already.
 
 another_one = Using('001', 'Sonic')
 
