db681360ffa2d241098a6ef9d1cc2df36d6b1c46
coderrob22 <robertknott7@gmail.com>
2023-07-10 04:43:03 -0400

    changing data bank to question bank

--- day17_project_quizGame.py
+++ day17_project_quizGame.py
@@ -5,7 +5,6 @@ from quiz_game_data import question_data
 question_bank = []
 
 for items in question_data:
-    question_bank.append(items)
-
-
-question_obj = Question(items['text'], items['answer'])
+    
+    question_obj = Question(items['text'], items['answer'])
+    question_bank.append(question_obj)
