4cecee3deef80e3ba603b33b1e5cf64e1fd16761
coderrob22 <robertknott7@gmail.com>
2023-08-02 04:43:03 -0400

    drawing square with Turtle module

--- day18_Turtle and the GUI.py
+++ day18_Turtle and the GUI.py
@@ -2,13 +2,16 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 
-tim.shape('turtle')
-tim.color('gold')
-tim.forward(100)
-tim.right(90)
+# tim.shape('turtle')
+# tim.color('gold')
+# tim.forward(100)
+# tim.right(90)
 
+#Draw a square
 
-
+for _ in range(4):
+    tim.forward(100)
+    tim.left(90)
 
 
 
