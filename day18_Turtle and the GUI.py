de8b2c21361e086094fdb8cdc232cfe3c78e85e3
coderrob22 <robertknott7@gmail.com>
2023-08-05 04:43:03 -0400

    function to draw shapes that increment within a certain range

--- day18_Turtle and the GUI.py
+++ day18_Turtle and the GUI.py
@@ -13,12 +13,20 @@ tim = Turtle()
 #     tim.forward(100)
 #     tim.left(90)
 
-for _ in range(5):    
-    tim.forward(10)
-    tim.up()
-    tim.forward(10)
-    tim.down()
+# for _ in range(5):    
+#     tim.forward(10)
+#     tim.up()
+#     tim.forward(10)
+#     tim.down()
 
+def draw_shape(num_sides):
+    angle = 360/num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    draw_shape(shape_side_n)
 
 screen = Screen()
 screen.exitonclick()
