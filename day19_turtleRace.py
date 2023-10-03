d2ff9abd7220dec0a9566c3378cec06f3a019841
coderrob22 <robertknott7@gmail.com>
2023-10-03 09:51:07 -0400

    updated turtle race to loop instead of hard code

--- day19_turtleRace.py
+++ day19_turtleRace.py
@@ -5,31 +5,13 @@ screen = Screen()
 screen.setup(width=500, height=400)
 user_guess = screen.textinput(title='Make a Bet!', prompt='Who will win the race')
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+y_coordinate = [0, 30, -30, 60, -60, 90]
 
-tim = Turtle(shape='turtle')
-tim.penup()
-tim.goto(-230, 0)
-
-ben = Turtle(shape='turtle')
-ben.penup()
-ben.goto(-230, 20)
-
-kim = Turtle(shape='turtle')
-kim.penup()
-kim.goto(-230, -20)
-
-jill = Turtle(shape='turtle')
-jill.penup()
-jill.goto(-230, 40)
-
-steve = Turtle(shape='turtle')
-steve.penup()
-steve.goto(-230, -40)
-
-jack = Turtle(shape='turtle')
-jack.penup()
-jack.goto(-230, 60)
-
+for x in range(0, 6):
+    tim = Turtle(shape='turtle')
+    tim.penup()
+    tim.color(colors[x])
+    tim.goto(x=-230, y= y_coordinate[x])
 
 
 
