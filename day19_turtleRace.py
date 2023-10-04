9a56c553144f3be21594156e066af95e46b96cd7
coderrob22 <robertknott7@gmail.com>
2023-10-04 09:51:07 -0400

    turtles differ by color and race at different speeds

--- day19_turtleRace.py
+++ day19_turtleRace.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 
 screen = Screen()
@@ -6,14 +7,21 @@ screen.setup(width=500, height=400)
 user_guess = screen.textinput(title='Make a Bet!', prompt='Who will win the race')
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 y_coordinate = [0, 30, -30, 60, -60, 90]
+all_turtles = []
 
 for x in range(0, 6):
-    tim = Turtle(shape='turtle')
-    tim.penup()
-    tim.color(colors[x])
-    tim.goto(x=-230, y= y_coordinate[x])
+    new_turtle = Turtle(shape='turtle')
+    new_turtle.penup()
+    new_turtle.color(colors[x])
+    new_turtle.goto(x=-230, y= y_coordinate[x])
+    all_turtles.append(new_turtle)
 
+if user_guess:
+    is_race_on = True
 
-
+while is_race_on:
+    for turtle in all_turtles:
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 screen.exitonclick()
