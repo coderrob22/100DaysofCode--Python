c5f68fec6e8f53874263cd5e8e8ac566061917f3
coderrob22 <robertknott7@gmail.com>
2023-10-08 09:51:07 -0400

    OOP has been inititialized and the snake object created with different snake methods

---
+++ snake_game_snake.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0,0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+class Snake: 
+
+    def __init__(self) -> None:
+        self.segments =[]
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            snake = Turtle('square')
+            snake.color('white')
+            snake.penup()
+            snake.goto(position)
+            self.segments.append(snake)
+
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
