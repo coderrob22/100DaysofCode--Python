164340d298d5effd6de63129415e3e2bf35d8149
coderrob22 <robertknott7@gmail.com>
2023-10-10 09:51:07 -0400

    the snake can now move based on the directional pad being pressed

--- day20_snakeGame.py
+++ day20_snakeGame.py
@@ -12,6 +12,13 @@ screen.tracer(0)
 
 snake = Snake()
 
+# Create event listeners to listen to arrow input-- This is how we control the snake's movement
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_is_on = True
 while game_is_on:
     screen.update()
--- snake_game_snake.py
+++ snake_game_snake.py
@@ -8,6 +8,7 @@ class Snake:
     def __init__(self) -> None:
         self.segments =[]
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -24,4 +25,16 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
 
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.head.setheading(90)
+
+    def down(self):
+        self.segments[0].setheading(270)
+
+    def left(self):
+        self.segments[0].setheading(180)
+
+    def right(self):
+        self.segments[0].setheading(0)
