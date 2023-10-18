91c931e4f0b0ff23791e4fef3244fb2435869ce8
coderrob22 <robertknott7@gmail.com>
2023-10-18 09:51:07 -0400

    snake can now eat food and the food will reappear somwhere else

--- day20_snakeGame.py
+++ day20_snakeGame.py
@@ -28,8 +28,11 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # Logic to detect when the snake collides with the food
+    if snake.head.distance(food) < 15:
+        food.refresh_food()
 
     
 screen.exitonclick()
--- snake_game_food.py
+++ snake_game_food.py
@@ -4,11 +4,13 @@ import random
 class Food(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape('cirlce')
+        self.shape('circle')
         self.penup()
         self.shapesize(stretch_len=.5, stretch_wid=.5)
         self.color('blue')
         self.speed('fastest')
+
+    def refresh_food(self):
         y_coordinate = random.randint(-280, 280)
         x_coordinate = random.randint(-280, 280)
         self.goto(x_coordinate, y_coordinate)
