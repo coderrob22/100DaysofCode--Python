2cbaf01eb5bc5b93ed2feac7c435ae874877585e
coderrob22 <robertknott7@gmail.com>
2023-10-20 09:51:07 -0400

    wall detection, which triggers "game over", has been added

--- day20_snakeGame.py
+++ day20_snakeGame.py
@@ -35,5 +35,10 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh_food()
         scoreboard.increase_score()
+
+    # Logic to detect if the snake hits the wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
     
 screen.exitonclick()
