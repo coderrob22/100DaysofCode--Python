76a4c5f279d6ea8f7e71990be99c6a04411298a7
coderrob22 <robertknott7@gmail.com>
2023-10-23 09:51:07 -0400

    refactored the code using slicing

--- day20_snakeGame.py
+++ day20_snakeGame.py
@@ -45,10 +45,8 @@ while game_is_on:
     
     # GAME OVER 2
     # Logic to detect if snake runs into any part of itself
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on= False
             scoreboard.game_over()
 
