da00a39a360fdf7ca4e3386a27f731cd65c99d78
coderrob22 <robertknott7@gmail.com>
2023-11-12 08:45:03 -0500

    Pong OOP, Ball class was created and instance was called

--- day22_pong.py
+++ day22_pong.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from pong_paddle import Paddle
+from pong_ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -13,6 +15,9 @@ right_paddle = Paddle((350, 0))
 #2nd Paddle instance
 left_paddle = Paddle((-350, 0))
 
+# Ball
+ball = Ball()
+
 
 screen.listen()
 screen.onkey(right_paddle.go_up, 'Up')
@@ -23,6 +28,8 @@ screen.onkey(left_paddle.go_down, 's')
 game_is_on = True
 
 while game_is_on:
+    time.sleep(.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
