930b0ab87043ecb5f7908a5d5817f010f552d9e6
coderrob22 <robertknott7@gmail.com>
2023-11-21 08:45:03 -0500

    Too many cars were being generated so the random module was used to create a car every 6th instance

--- turtleCrossing_car_manager.py
+++ turtleCrossing_car_manager.py
@@ -10,14 +10,16 @@ class Car_manager():
         self.all_cars =[]
 
     def create_car(self):
-        new_car = Turtle('square')
-        new_car.penup()
-        new_car.shapesize(stretch_len=1, stretch_wid=2)
-        new_car.color(random.choice(COLORS))
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle('square')
+            new_car.penup()
+            new_car.shapesize(stretch_len=2, stretch_wid=1)
+            new_car.color(random.choice(COLORS))
 
-        random_y_range = random.randint(-250, 250)
-        new_car.goto(300, random_y_range)
-        self.all_cars.append(new_car)
+            random_y_range = random.randint(-250, 250)
+            new_car.goto(300, random_y_range)
+            self.all_cars.append(new_car)
 
     def move_cars(self):
         for car in self.all_cars:
