b4adc480c297cd5c8a406ce6bc1d15139e7003e3
coderrob22 <robertknott7@gmail.com>
2023-12-20 09:51:07 -0500

    more list comprehensions

--- day26_lists.py
+++ day26_lists.py
@@ -7,3 +7,18 @@ squared_numbers = [x * x for x in numbers]
 
 
 print(squared_numbers)
+
+###################################################
+
+list_of_strings = input().split(',')
+# 🚨 Do  not change the code above
+
+# TODO: Use list comprehension to convert the strings to integers 👇:
+result = [int(x) for x in list_of_strings if int(x) % 2 == 0]
+
+# TODO: Use list comprehension to filter out the odd numbers
+# and store the even numbers in a list called "result"
+
+
+# Write your code 👆 above:
+print(result)
