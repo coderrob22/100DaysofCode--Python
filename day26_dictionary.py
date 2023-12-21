84a282a834e7d29f7d60579d0fc9622fb53f12f5
coderrob22 <robertknott7@gmail.com>
2023-12-21 09:51:07 -0500

    more dict comprehension

--- day26_dictionary.py
+++ day26_dictionary.py
@@ -3,3 +3,7 @@ names = {'Alex', 'Beth', 'Caroline', 'Dave', 'Eve', 'Freddie'}
 import random
 
 student_scores ={student:random.randint(1, 100) for student in names}
+
+# passed_scores = {new_key:new_value for (key, value) in dictionary.items()}
+
+passed_scores = {student:score for (student, score) in student_scores.items() if score >= 60}
