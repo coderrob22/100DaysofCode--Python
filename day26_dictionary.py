2003b02afad9644d4c383f34cff09f66951bed61
coderrob22 <robertknott7@gmail.com>
2023-12-22 09:51:07 -0500

    interactive dictionary comprehension

--- day26_dictionary.py
+++ day26_dictionary.py
@@ -7,3 +7,15 @@ student_scores ={student:random.randint(1, 100) for student in names}
 # passed_scores = {new_key:new_value for (key, value) in dictionary.items()}
 
 passed_scores = {student:score for (student, score) in student_scores.items() if score >= 60}
+
+###################################################
+
+sentence = input()
+# 🚨 Don't change code above 👆
+# Write your code below 👇
+
+list = sentence.split()
+result = {word:len(word) for word in list}
+
+
+print(result)
